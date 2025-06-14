59162ccc783f4e0fbd6ce03bd612efbac8232c3d
Arthuranjo <arthurdosanjosnascimento@gmail.com>
2025-06-13 21:01:59 -0300

    removendo a linha 29 do requirements

--- database/db.py
+++ database/db.py
@@ -14,7 +14,7 @@ def get_db_config():
     return {
         'host': os.getenv('DB_HOST', 'localhost'),
         'user': os.getenv('DB_USER', 'root'),
-        'password': os.getenv('DB_PASSWORD', 'arthur'),
+        'password': os.getenv('DB_PASSWORD', 'root'),
         'database': os.getenv('DB_NAME', 'charles_pizzaria')
     }
 
