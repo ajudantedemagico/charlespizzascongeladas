1fd4bf0d977469ffdf0b70cfd18b18d42e19ef95
ajudantedemagico <jqlngnclvz@gmail.com>
2025-06-18 09:46:09 -0300

    Ajustes em imagens e comentários

--- static/JS/carrossel-cardapio.js
+++ static/JS/carrossel-cardapio.js
@@ -3,14 +3,14 @@ function trocarPizza(element) {
     const desc = element.getAttribute('data-desc');
     const imgSrc = element.getAttribute('src');
 
-    // Atualiza imagem, título e descrição
+    // Atualizar a imagem, título e descrição
     document.getElementById('pizza-img').src = imgSrc;
     document.getElementById('pizza-title').textContent = sabor;
     document.getElementById('pizza-desc').textContent = desc;
 
-    // Atualiza o input hidden do sabor (para enviar no formulário)
+    // Atualiza o input hidden do sabor -> para enviar no formulário
     document.getElementById('input-sabor').value = sabor;
 
-    // Zera todas as quantidades ao mudar o sabor (opcional)
+    // Zerar todas as quantidades ao mudar o sabor 
     document.querySelectorAll('input[name="quantidades"]').forEach(input => input.value = 0);
 }
